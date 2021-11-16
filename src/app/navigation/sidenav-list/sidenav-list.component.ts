0c3d298a0b743ebb3d1020b4d209b863fe6bbe57
DESKTOP-B421EOF\admin <deepak.bawa@orionesolutions.in>
2021-11-16 15:30:55 +0530

    working on auth

--- src/app/navigation/sidenav-list/sidenav-list.component.ts
+++ src/app/navigation/sidenav-list/sidenav-list.component.ts
@@ -1,21 +1,36 @@
-import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output, OnDestroy } from '@angular/core';
+import { AuthService } from 'src/app/auth/auth.service';
+
+import { Subscription } from "rxjs";
 
 @Component({
   selector: 'app-sidenav-list',
   templateUrl: './sidenav-list.component.html',
   styleUrls: ['./sidenav-list.component.scss']
 })
-export class SidenavListComponent implements OnInit {
+export class SidenavListComponent implements OnInit, OnDestroy {
 
   @Output() closeSidenav = new EventEmitter();
+  @Output() sidenavToggle = new EventEmitter<void>();
+  isAuth: boolean = false;
+  authSubscription$!: Subscription;
 
-  constructor() { }
+  constructor(
+    private authService: AuthService
+  ) { }
 
   ngOnInit(): void {
+    this.authSubscription$ = this.authService.authChange$.subscribe( (authStatus) => {
+      this.isAuth = authStatus;
+    } );
   }
 
   onClose() {
     this.closeSidenav.emit();
   }
 
+  ngOnDestroy() {
+
+    this.authSubscription$.unsubscribe();
+  }
 }
