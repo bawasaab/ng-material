c36908bc764d5839bd1605dae57cb3ddae2bfc97
DESKTOP-B421EOF\admin <deepak.bawa@orionesolutions.in>
2021-11-18 17:05:25 +0530

    build

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,14 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBNWcY8dOXQ7Nm4hsfEO07YTPnfP6LbqBU",
+  authDomain: "ng-fitness-tracker-3d099.firebaseapp.com",
+  projectId: "ng-fitness-tracker-3d099",
+  storageBucket: "ng-fitness-tracker-3d099.appspot.com",
+  messagingSenderId: "150905689374",
+  appId: "1:150905689374:web:51ef17ef5c738a280e250f",
+  measurementId: "G-9CVKWVJ256"
+};
+
 export const environment = {
-  production: true
+  production: true,
+  firebase: firebaseConfig
 };
